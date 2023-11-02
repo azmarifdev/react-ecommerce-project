71f449d5bdc9d9a169dbd04e6750acbddf44f3cc
azmarifdev <azmarif.dev@gmail.com>
2023-11-03 03:37:53 +0600

    project push

--- src/components/NavItems.jsx
+++ src/components/NavItems.jsx
@@ -1,9 +1,76 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../assets/images/logo/logo.png';
 
 const NavItems = () => {
-  return (
-    <div>NavItems</div>
-  )
-}
+    const [menuToggle, setMenuToggle] = useState(false);
+    const [socialToggle, setSocialToggle] = useState(false);
+    const [headerFixed, setHeaderFixed] = useState(false);
 
-export default NavItems
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 200) {
+            setHeaderFixed(true);
+        } else {
+            setHeaderFixed(false);
+        }
+    });
+
+    return (
+        <>
+            <header className={`header-section style-4 ${headerFixed ? 'header-fixed fadeInUp' : ''}`}>
+                {/* header top start */}
+                <div className={`header-top d-md-none ${socialToggle ? 'open' : ''}`}>
+                    <div className="container">
+                        <div className="header-top-area">
+                            <Link to="/signup" className="lab-btn me-3">
+                                <span>Create Account</span>
+                            </Link>
+                            <Link to="/login">Log In</Link>
+                        </div>
+                    </div>
+                </div>
+
+                {/* header bottom */}
+                <div className="header-bottom">
+                    <div className="container">
+                        <div className="header-wrapper">
+                            {/* logo */}
+                            <div className="logo-search-acte">
+                                <div className="logo">
+                                    <Link to="/">
+                                        <img src={logo} alt="logo" />
+                                    </Link>
+                                </div>
+                            </div>
+                            {/* menu area */}
+                            <div className="menu-area">
+                                <div className="menu">
+                                    <ul className={`lab-ul ${menuToggle ? 'active' : ''}`}>
+                                        <li>
+                                            <Link to="/">Home</Link>
+                                        </li>
+                                        <li>
+                                            <Link to="/shop">Shop</Link>
+                                        </li>
+                                        <li>
+                                            <Link to="/blog">Blog</Link>
+                                        </li>
+                                        <li>
+                                            <Link to="/about">About</Link>
+                                        </li>
+                                        <li>
+                                            <Link to="/contact">Contact</Link>
+                                        </li>
+                                    </ul>
+                                </div>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </header>
+        </>
+    );
+};
+
+export default NavItems;
