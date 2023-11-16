1409dc8a0ef5894071ed4d82fca88489134c0598
azmarifdev <azmarif.dev@gmail.com>
2023-11-16 23:59:04 +0600

    again

--- src/shop/Shop.jsx
+++ src/shop/Shop.jsx
@@ -1,19 +1,25 @@
 import React, { useState } from 'react';
 import PageHeader from '../components/PageHeader';
+import Data from '../products.json'
+import ProductCards from './ProductCards';
+
 const showResult = 'Showing 01 - 12 of 139 Results';
+
+
 const Shop = () => {
     const [GridList, setGridList] = useState(true);
-
-    console.log(GridList);
+    const [products, setProducts] = useState(Data)
+    console.log(products);
     return (
         <div>
-            <PageHeader title={'Our Shop Page'} curPage={'Shop'} /> 
+            <PageHeader title={'Our Shop Page'} curPage={'Shop'} />
             {/* shop page */}
             <div className="shop-page padding-tb">
                 <div className="container">
                     <div className="row justify-content-center">
                         <div className="col-lg-8 col-12">
                             <article>
+                                {/* layout and title here */}
                                 <div className="shop-title d-flex flex-wrap justify-content-between">
                                     <p>{showResult}</p>
                                     <div className={`product-view-mode ${GridList ? 'gridActive' : 'listActive'}`}>
@@ -25,8 +31,13 @@ const Shop = () => {
                                         </a>
                                     </div>
                                 </div>
+                                {/* products cards */}
+                                <div>
+                                    <ProductCards gridList={GridList} products={products} />
+                                </div>
                             </article>
                         </div>
+                        <div className="col-lg-4 col-12">right side</div>
                     </div>
                 </div>
             </div>
