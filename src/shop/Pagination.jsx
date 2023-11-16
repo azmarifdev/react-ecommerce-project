6ade41b92a5299edf570cb1111bc2461e732cd42
azmarifdev <azmarif.dev@gmail.com>
2023-11-17 05:39:30 +0600

    pagination style css

--- src/shop/Pagination.jsx
+++ src/shop/Pagination.jsx
@@ -1,6 +1,13 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 
-const Pagination = () => {
+const Pagination = ({ activePage, totalProducts, handlePageChange, productPerPage }) => {
+    const pageNumber = [];
+    console.log(pageNumber);
+    for (let i = 1; i <= Math.ceil(totalProducts / productPerPage); i++) {
+        pageNumber.push(i);
+    }
+
     return (
         <div>
             <h1>pagination</h1>
