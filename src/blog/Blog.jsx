5ec61ae32ae199a54eea8947c8f7c81eeba95662
azmarifdev <azmarif.dev@gmail.com>
2023-12-05 21:04:41 +0600

    some

--- src/blog/Blog.jsx
+++ src/blog/Blog.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import PageHeader from '../components/PageHeader'
+import React from 'react';
+import PageHeader from '../components/PageHeader';
 
 const Blog = () => {
-  return (
-    <div>
-      <PageHeader title={'Blog Page'} curPage={'Blog'} />
-    </div>
-  )
-}
+    return (
+        <div>
+            <PageHeader title={'Blog Page'} curPage={'Blog'} />
+        </div>
+    );
+};
 
-export default Blog
+export default Blog;
