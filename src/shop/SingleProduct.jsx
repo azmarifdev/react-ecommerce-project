a407c22ebb4a9e121b823496decdeed981fde39a
azmarifdev <azmarif.dev@gmail.com>
2023-12-05 21:43:04 +0600

    some chang

--- src/shop/SingleProduct.jsx
+++ src/shop/SingleProduct.jsx
@@ -1,22 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Data from '../products.json';
+import PageHeader from '../components/PageHeader';
 
 const SingleProduct = () => {
-    const [product, setProduct] = useState(Data);
+    const [product, setProduct] = useState([]);
 
     const { id } = useParams();
-    console.log(product);
-    console.log(id);
+    // console.log(id);
 
     useEffect(() => {
         fetch('../products.json')
             .then((res) => res.json())
             .then((data) => setProduct(data));
     }, []);
-    console.log(product);
+    // console.log(product);
 
-    return <div>SingleProduct</div>;
+    return (
+        <div>
+            <PageHeader title={'OUR SINGLE PAGE'} curPage={'Shop/Single Page'} />
+        </div>
+    );
 };
 
 export default SingleProduct;
