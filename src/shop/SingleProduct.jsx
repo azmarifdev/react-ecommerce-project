fd3aa55f9c880341083e5b84a0cc5fecf26f7a50
azmarifdev <azmarif.dev@gmail.com>
2023-12-05 22:48:45 +0600

    some chang

--- src/shop/SingleProduct.jsx
+++ src/shop/SingleProduct.jsx
@@ -19,6 +19,26 @@ const SingleProduct = () => {
     return (
         <div>
             <PageHeader title={'OUR SINGLE PAGE'} curPage={'Shop/Single Page'} />
+            <div className="shop-single padding-tb aside-bg">
+                <div className="container">
+                    <div className="row justify-content-center">
+                        <div className="col-lg-8 col-12">
+                            <article>
+                                <div className="product-details">
+                                    <div className="row align-items-center">
+                                        <div className="col-md-6 col-12">Images</div>
+                                        <div className="col-md-6 col-12">Details</div>
+                                    </div>
+                                </div>
+                                {/* Review */}
+                                <div className="review">Review</div>
+                            </article>
+                        </div>
+                        {/* right side */}
+                        <div className="col-lg-4 col-12">Right Side</div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
